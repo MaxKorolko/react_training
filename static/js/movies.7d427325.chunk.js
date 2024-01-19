4738c0b03f58e1caf3f73828051f4ca4ff027bf8
wla-maksym-ko <143793225+wla-maksym-ko@users.noreply.github.com>
2024-01-19 14:37:26 +0000

    Deploying to gh-pages from @ MaxKorolko/react_training@d3c308faa889538af1b03d5d2761649f0ab70b78 🚀

--- static/js/movies.7d427325.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_training=self.webpackChunkreact_training||[]).push([[936],{764:(e,n,r)=>{r.r(n),r.d(n,{default:()=>a});var i=r(184);function a(){return(0,i.jsx)("main",{children:(0,i.jsx)("h1",{children:"Hello Movies"})})}}}]);
-//# sourceMappingURL=movies.7d427325.chunk.js.map
